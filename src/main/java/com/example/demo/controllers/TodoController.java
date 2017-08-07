247fd9114e6153d7949a6a2ecec419ed91832ece
puku0x <puku0x@gmail.com>
2017-08-08 00:00:18 +0900

    cors

--- src/main/java/com/example/demo/controllers/TodoController.java
+++ src/main/java/com/example/demo/controllers/TodoController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.demo.entities.Todo;
 import com.example.demo.forms.TodoForm;
 import com.example.demo.services.TodoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/todoes")
 public class TodoController {
